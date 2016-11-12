0ebf47215c16bfd9702b2da1ad47e773d7d6abab
Karthik Shanmugam <karthiks@berkeley.edu>
2016-11-12 13:16:49 -0800

    add cooldown timer

--- FlaskWebProject1/static/main.js
+++ FlaskWebProject1/static/main.js
@@ -5,36 +5,80 @@ $(document).ready(function() {
     function collect_snapshots(delay, interval, frames) {
         $(".emotion-img").attr("src", "");
         images = [];
-        for (i = 0; i < frames; i++) { 
-            setTimeout(function(){
-                console.log("snap!!!");
-                Webcam.snap(function(data_uri) {images.push(data_uri);});
-            }, delay + (interval/frames)*i);
-        }
-        setTimeout(function() {
-            post("/images/uploads", {'images': JSON.stringify(images), 'content':$('.content').attr("src")}, function(data){
-                console.log(data);
-                switch (data) {
-                    case "happiness":
-                        $(".emotion-img").attr("src", "emojis/happy.png");
-                        break;
-                    case "sadness":
-                        $(".emotion-img").attr("src", "emojis/sad.png");
-                        break;
-                    case "anger":
-                        $(".emotion-img").attr("src", "emojis/angry.png");
-                        break;
-                    case "surprise":
-                        $(".emotion-img").attr("src", "emojis/surprised.png");
-                        break;
-                    case "neutral":
-                        $(".emotion-img").attr("src", "emojis/neutral.png");
-                        break;
-                    default:
-                        $(".emotion-img").attr("src", "emojis/missing.png");
-                }
-            });            
-        }, delay+interval+200);
+        // for (i = 0; i < frames; i++) { 
+        //     setTimeout(function(){
+        //         console.log("snap!!!");
+        //         Webcam.snap(function(data_uri) {images.push(data_uri);});
+        //     }, delay + (interval/frames)*i);
+        // }
+        $("#cd").cooldown({
+          tickFrequency:      50,        // Frequency of ticks (milliseconds), not recommended <50, affects
+                                         // countdown (and arc in non-Chrome browsers)
+          arcWidth:           10,        // Arc stroke width
+          arcColor:           "#27ae60", // Arc stroke color
+          arcBackgroundColor: "#d7d8d9", // Arc stroke unfinished color
+          toFixed:            1,         // Number of decimal places to show in countdown
+          introDuration:      0,       // Duration of spinning intro (milliseconds), set to 0 to disable
+          countdownCss:       {          // Object of CSS attributes (passed to jQuery's css() function)
+                                width: "100%",
+                                height: "100%",
+                                margin: 0,
+                                padding: 0,
+                                textAlign: "center",
+                                fontSize: "16px"
+                              },
+          completeFn:         function(){
+                console.log("snap!!!");Webcam.snap(function(data_uri) {images.push(data_uri);});
+                post("/images/uploads", {'images': JSON.stringify(images), 'content':$('.content').attr("src")}, function(data){
+                    console.log(data);
+                    switch (data) {
+                        case "happiness":
+                            $(".emotion-img").attr("src", "emojis/happy.png");
+                            break;
+                        case "sadness":
+                            $(".emotion-img").attr("src", "emojis/sad.png");
+                            break;
+                        case "anger":
+                            $(".emotion-img").attr("src", "emojis/angry.png");
+                            break;
+                        case "surprise":
+                            $(".emotion-img").attr("src", "emojis/surprised.png");
+                            break;
+                        case "neutral":
+                            $(".emotion-img").attr("src", "emojis/neutral.png");
+                            break;
+                        default:
+                            $(".emotion-img").attr("src", "emojis/missing.png");
+                    }
+                });   
+                },       // Callback function called when cooldown expires
+          countdownFn:        null       // Can be used to override how the countdown is displayed, is passed
+                                         // the remaining time in seconds
+        }).start(delay/1000.0);
+        // setTimeout(function() {
+        //     post("/images/uploads", {'images': JSON.stringify(images), 'content':$('.content').attr("src")}, function(data){
+        //         console.log(data);
+        //         switch (data) {
+        //             case "happiness":
+        //                 $(".emotion-img").attr("src", "emojis/happy.png");
+        //                 break;
+        //             case "sadness":
+        //                 $(".emotion-img").attr("src", "emojis/sad.png");
+        //                 break;
+        //             case "anger":
+        //                 $(".emotion-img").attr("src", "emojis/angry.png");
+        //                 break;
+        //             case "surprise":
+        //                 $(".emotion-img").attr("src", "emojis/surprised.png");
+        //                 break;
+        //             case "neutral":
+        //                 $(".emotion-img").attr("src", "emojis/neutral.png");
+        //                 break;
+        //             default:
+        //                 $(".emotion-img").attr("src", "emojis/missing.png");
+        //         }
+        //     });            
+        // }, delay+interval+200);
   
     }
 
